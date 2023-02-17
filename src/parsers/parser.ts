503ef69956a7effdbe696a7c7275fadba9a5d8fc
Mutsgen <danobez21@gmail.com>
2023-02-18 02:11:37 +0300

    Co-authored-by: Alex <pvk04@users.noreply.github.com> Co-authored-by: Exolan <Exolan@users.noreply.github.com>

--- src/parsers/parser.ts
+++ src/parsers/parser.ts
@@ -1,6 +1,15 @@
 import * as XLSX from 'xlsx';
 
-const parser = (columns, strings) => {
+function setNullAsZero(dataset: XLSX.WorkBook, datasetPages: string[]) {
+  const titles = returnAllTitle(dataset, datasetPages);
+  const stopColumn = String.fromCharCode('A'.charCodeAt(0) + titles.length);
+  const everyColumnOfTable = [];
+  for (let i = 0; i < titles.length; i++) {
+    everyColumnOfTable.push(String.fromCharCode('A'.charCodeAt(0) + i));
+  }
+}
+
+const parser = (columns: string[], strings: any) => {
   const array = [];
   let counter = 0;
   let object = {};
@@ -61,10 +70,21 @@ function returnAllTitle(dataset: XLSX.WorkBook, datasetPages: string[]) {
   return titles;
 }
 
-export async function parserToRegionData(dataset: XLSX.WorkBook) {
-  const datasetPages = Object.keys(dataset.Sheets);
+export async function parserToFirstSection(
+  dataset: XLSX.WorkBook,
+  page: string,
+) {
+  const datasetPages = [page || 'Р1'];
   const regions = returnAllRegions(dataset, datasetPages);
   const titles = returnAllTitle(dataset, datasetPages);
+  const stopColumn = String.fromCharCode('A'.charCodeAt(0) + titles.length);
+  const everyColumnOfTable = [];
+  for (let i = 0; i < titles.length; i++) {
+    everyColumnOfTable.push(String.fromCharCode('A'.charCodeAt(0) + i));
+  }
+  // console.log('stop', stopColumn);
+  // console.log(everyColumnOfTable);
+
   for (let i = 0; i < datasetPages.length; i++) {
     if (
       datasetPages[i].includes('Раздел') ||
@@ -72,15 +92,61 @@ export async function parserToRegionData(dataset: XLSX.WorkBook) {
     ) {
       continue;
     }
-    const page = dataset.Sheets[datasetPages[i]];
-
-    for (const el in page) {
-      if (el[0] === 'A' && el !== 'A1') {
-        if (!regions.includes(page[el].v)) regions.push(page[el].v);
+    const page = Object.entries(dataset.Sheets[datasetPages[i]]);
+    const newArray = [];
+    let counterString = 2;
+    let counterColumn = 0;
+    let cell = everyColumnOfTable[counterColumn] + counterString;
+    for (let j = 0; j < 200; j++) {
+      const element = page[j];
+      if (
+        element[0].includes('!') ||
+        titles.includes(element[1].v) ||
+        element[0].includes(stopColumn)
+      ) {
+        continue;
       }
+      if (
+        element[0].includes(stopColumn) ||
+        counterColumn >= everyColumnOfTable.length
+      ) {
+        counterString++;
+        counterColumn = 0;
+        cell = everyColumnOfTable[counterColumn] + counterString;
+      }
+      // console.log(
+      //   cell,
+      //   element[0],
+      //   counterColumn,
+      //   everyColumnOfTable[counterColumn],
+      // );
+      if (cell != element[0]) {
+        console.log(cell, element[0]);
+        newArray.push(0);
+        counterColumn++;
+      }
+
+      newArray.push(element[1].v);
+
+      if (
+        element[0].includes(stopColumn) ||
+        counterColumn >= everyColumnOfTable.length
+      ) {
+        counterString++;
+        counterColumn = 0;
+        cell = everyColumnOfTable[counterColumn] + counterString;
+      }
+
+      counterColumn++;
+      cell = everyColumnOfTable[counterColumn] + counterString;
     }
+
+    const mainArray = parser(titles, newArray);
+    // console.log(mainArray);
+    // console.log(titles, newArray);
+
+    return mainArray;
   }
-  return { titles };
 }
 
 export async function parseFromYearStatistic(dataset: XLSX.WorkBook) {
@@ -88,10 +154,8 @@ export async function parseFromYearStatistic(dataset: XLSX.WorkBook) {
   for (let i = 0; i < datasetPages.length; i++) {
     if (datasetPages[i] === 'статистика по годам') {
       const page = dataset.Sheets[datasetPages[i]];
-      // console.log(page);
       const array = Object.entries(page);
-      // console.log(array);
-      const arrayColumnsName = ['Фактор'];
+      const arrayColumnsName = ['Factor'];
       const newArray = [];
       for (const el in array) {
         const element = array[el];
@@ -114,7 +178,6 @@ export async function parseFromYearStatistic(dataset: XLSX.WorkBook) {
       }
 
       const mainArray = parser(arrayColumnsName, newArray);
-      // console.log(mainArray);
       return mainArray;
     }
   }
